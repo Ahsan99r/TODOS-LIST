5621121f21102dc77dd90633f5ce3253b3f48c99
M AHSAN ARSHAD <165573133+Ahsan99r@users.noreply.github.com>
2024-06-04 15:28:53 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer"
+
+let todoList=[]
+let condition=true;
+while(condition){
+let addTask =await inquirer.prompt(
+   [
+      {
+   name:"todo",
+   type:"input",
+   message:"What do you want add in your TODO list?",
+
+},
+{
+   name:"addMore",
+   type:"confirm",
+   message:"Do you want to add more?",
+   default:true,
+},
+
+]
+);
+todoList.push(addTask.todo)
+condition=addTask.addMore
+console.log (todoList)
+}
+
